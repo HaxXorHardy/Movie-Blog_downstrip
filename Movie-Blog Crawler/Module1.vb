55df778356bbe84cb099aa959a9bb3cdf196e5de
Hardy <haxxorhardy@gmail.com>
2017-02-01 11:16:06 +0100

    added comments but not enugh

--- Movie-Blog Crawler/Module1.vb
+++ Movie-Blog Crawler/Module1.vb
@@ -7,58 +7,50 @@ Imports System.Text.RegularExpressions
 
 Module Module1
 
-#Region "Public_Dims"
+#Region "Public_Dims"                                                                               'i think here is nothing to say
+
     'Dim dt As DateTime = Date.Today.AddDays(-2) 'for Testing
     Dim dt As DateTime = Date.Today
     Dim month As String = dt.ToString("MM", CultureInfo.InvariantCulture)
     Dim year As String = dt.ToString("yyyy", CultureInfo.InvariantCulture)
     Dim day As String = dt.ToString("dd", CultureInfo.InvariantCulture)
     Dim pages As Integer = 0
-    Const quote As String = """"
-
-    Public spiderurl = "http://www.movie-blog.org/" & year & "/" & month & "/" & day & "/"
+    Const quote As String = """"                                                                    'quote(") for text maybe char(34) is better but this works also :D
+    Public spiderurl = "http://www.movie-blog.org/" & year & "/" & month & "/" & day & "/"          'the mainurl from Today 
     Public list As New ArrayList
-
 
 #End Region
 
 
     Sub Main()
-
-        Console.WriteLine(spiderurl, Environment.NewLine)
-        pFinder()
+        pFinder()                                                                                   'Find numbrs of pages from Date.Today
 
         Try
             Dim i As Integer = 1
             While i <= pages
-                Dim aList As ArrayList = getShit(spiderurl & "page/" & i & "/")
+                Dim aList As ArrayList = getShit(spiderurl & "page/" & i & "/")                     'Take every page and search for Links
                 For Each item As String In aList
-                    If Not list.Contains(item) Then
-                        If item.Contains("720p") Or item.Contains("1080p") Then
-                            list.Add(item)
+                    If Not list.Contains(item) Then                                                 'check if item is allready in list
+                        If item.Contains("720p") Or item.Contains("1080p") Then                     'only choose HD movies or series
+                            list.Add(item)                                                          'add the item
                         End If
                     End If
                 Next
-                i += 1
+                i += 1                                                                              'Next Site
             End While
 
-
-
-            Dim file As System.IO.StreamWriter
+            Dim file As System.IO.StreamWriter                                                      'write the links to file(only for testing?)
             file = My.Computer.FileSystem.OpenTextFileWriter("c:\temp\MyTest.html", True)
             For Each item As String In list
                 Console.WriteLine(item, Environment.NewLine)
-                Dim trime As String = Replace(item, spiderurl, "")
-                file.WriteLine("<p><a href=" & quote & item & quote & ">" & trime & "</a></p>")
+                Dim trim As String = Replace(item, spiderurl, "")                                   'removes the main url from string (better Reading)
+                file.WriteLine("<p><a href=" & quote & item & quote & ">" & trim & "</a></p>")      'for html Sytax in File(needs mor testing)
             Next
-            file.Close()
-
-
-
+            file.Close()                                                                            'close the Streamwriter
 
         Catch ex As Exception
         End Try
-        Console.ReadLine()
+        Console.ReadLine()                                                                          'let the Consolewindow stay open
     End Sub
 
     Private Sub pFinder()
