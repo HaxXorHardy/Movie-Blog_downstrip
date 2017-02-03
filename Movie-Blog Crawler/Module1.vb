df3731b80c2fdf287b61e1192459b6bb5e9a5309
Hardy <haxxorhardy@gmail.com>
2017-02-03 13:00:03 +0100

    -added setting file -cleanup code -more function -less comments :D

--- Movie-Blog Crawler/Module1.vb
+++ Movie-Blog Crawler/Module1.vb
@@ -1,71 +1,88 @@
-﻿
-Imports System.Globalization
+﻿Imports System.Globalization
 Imports System.IO
 Imports System.Net
 Imports System.Text.RegularExpressions
+Imports System.Xml
 
 Module Module1
 
 #Region "Public_Dims"                                                                               'i think here is nothing to say
 
-    'Dim dt As DateTime = Date.Today.AddDays(-2) 'for Testing
-    'Dim gApiK As String = "AIzaSyC0WgLkbntFiBylDoSoYwPFZZVu2PETT_U"                                'googleapi key  key=API_KEY
-    'Dim sEngine As String = "000363274925813725598:6h3f2fhmhby"                                    'serchengine ID
-    Dim dt As DateTime = Date.Today
+    Dim dt As DateTime = Date.Today.AddDays(-2) 'for Testing
+    'Dim dt As DateTime = Date.Today
     Dim month As String = dt.ToString("MM", CultureInfo.InvariantCulture)
     Dim year As String = dt.ToString("yyyy", CultureInfo.InvariantCulture)
     Dim day As String = dt.ToString("dd", CultureInfo.InvariantCulture)
     Dim pages As Integer = 0
     Const quote As String = Chr(34)                                                                 'quote(") for text 
     Public spiderurl = "http://www.movie-blog.org/" & year & "/" & month & "/" & day & "/"          'the mainurl from Today 
-    Public list As New ArrayList
+    Dim nl = Environment.NewLine
 
 #End Region
 
     Sub Main()
-        pFinder()                                                                                    'Find numbrs of pages from Date.Today
         Try
-            Dim i As Integer = 1
-            While i <= pages
-                Dim aList As ArrayList = getShit(spiderurl & "page/" & i & "/")                      'Take every page and search for Links
-                For Each item As String In aList
-                    If Not list.Contains(item) Then                                                  'check if item is allready in list
-                        If item.Contains("720p") Or item.Contains("1080p") Then                      'only choose HD movies or series
-                            If item.Contains("bdrip") Or item.Contains("bluray") Then
-                                If Not item.Contains("s0") Or Not item.Contains("s1") Or Not item.Contains("s2") Then
-                                    list.Add(item)                                                           'add the item
-                                End If
-                            End If
-                        End If
-                    End If
-                Next
-                i += 1                                                                               'Next Site
-            End While
-
-            'Dim file As System.IO.StreamWriter                                                      'write the links to file(only for testing?)
-            'file = My.Computer.FileSystem.OpenTextFileWriter("c:\temp\MyTest.html", True)
+            xmlTesting()
+            Console.ReadLine()
+            pFinder()                                                                                       'Find numbrs of pages from Date.Today
+            Dim listH As New ArrayList
+            listH = listHelper()                                                                            'creates a link list from pages
             Dim outP As New ArrayList
             Dim x As Integer = 1
-            For Each item As String In list
+            For Each item As String In listH
                 Dim retStr As String = findTitle(item)
                 If Not retStr = Nothing Then
                     If Not outP.Contains(retStr) Then
                         outP.Add(retStr)
-                        Console.WriteLine((String.Format("{0:000}", x) & ": " & retStr), Environment.NewLine)
+                        Console.WriteLine((String.Format("{0:000}", x) & ": " & retStr), nl)
+                        writeFile(item.ToString, retStr)
                         x += 1
                     End If
                 End If
-                'Console.WriteLine(item, Environment.NewLine)
-                'Dim trim As String = Replace(item, spiderurl, "")                                   'removes the main url from string (better Reading)
-                'file.WriteLine("<p><a href=" & quote & item & quote & ">" & trim & "</a></p>")      'for html Sytax in File(needs mor testing)
             Next
-            'file.Close()                                                                            'close the Streamwriter
+
+            Console.WriteLine("Ende.....")
+            Console.ReadLine()                                                                                  'let the Consolewindow stay open
+            File.Delete("c:\temp\MyTest.html")
         Catch ex As Exception
-            Console.WriteLine(ex.Message, Environment.NewLine)                                       'Errorhandling for user debugging
+            Console.WriteLine(ex.Message, nl)                                                               'Errorhandling for user debugging
             Console.ReadLine()
         End Try
-        Console.WriteLine("Ende.....")
-        Console.ReadLine()                                                                           'let the Consolewindow stay open
+    End Sub
+
+    Private Function listHelper()
+        Dim aReturn As New ArrayList
+        Dim i As Integer = 1
+        While i <= pages
+            Dim aList As ArrayList = getShit(spiderurl & "page/" & i & "/")                                         'Take every page and search for Links
+            For Each item As String In aList
+                If Not aReturn.Contains(item) Then                                                                  'check if item is allready in list
+                    If item.Contains("720p") Or item.Contains("1080p") Then                                         'only choose HD movies or series
+                        If item.Contains("bdrip") Or item.Contains("bluray") Then
+                            If Not item.Contains("s0") Or Not item.Contains("s1") Or Not item.Contains("s2") Then
+                                aReturn.Add(item)                                                                   'add the item
+                            End If
+                        End If
+                    End If
+                End If
+            Next
+            i += 1                                                                                                  'Next Site
+        End While
+        Return aReturn
+    End Function
+
+    Private Sub writeFile(ByVal strUrl As String, ByVal strName As String)
+        Dim filePath As String = "c:\temp\MyTest.html"
+        Dim fileWrite As System.IO.StreamWriter                                                                  'write the links to file(only for testing?)
+        If Not File.Exists(filePath) Then
+            fileWrite = My.Computer.FileSystem.OpenTextFileWriter(filePath, True)
+            fileWrite.WriteLine("<h1>Filme " & dt.Date & "</h1>")
+            fileWrite.Close()
+        End If
+        fileWrite = My.Computer.FileSystem.OpenTextFileWriter(filePath, True)
+        'Console.WriteLine(str, nl)
+        fileWrite.WriteLine("<p><a href=" & quote & strUrl & quote & ">" & strName & "</a></p>")                       'for html Sytax in File(needs mor testing)
+        fileWrite.Close()                                                                                        'close Streamwriter
     End Sub
 
     Private Sub pFinder()
@@ -81,12 +98,12 @@ Module Module1
             Dim HrefMatch As Match = HrefRegex.Match(strSource)                                                     'Find matches in String
             While HrefMatch.Success = True                                                                          'String Found = True 1 or more strings matching
                 Dim num As String = HrefMatch.Groups(1).Value                                                       'get first match
-                Console.WriteLine("Pages: " & num, Environment.NewLine)                                             'output for testing
+                Console.WriteLine("Pages: " & num, nl)                                                              'output for testing
                 pages = Convert.ToInt32(num)                                                                        'convert String to Integer
                 HrefMatch = HrefMatch.NextMatch                                                                     'More matches? get them and print this shit out
             End While
         Catch ex As Exception
-            Console.WriteLine(ex.Message, Environment.NewLine)                                                      'Errorhandling for user debugging
+            Console.WriteLine(ex.Message, nl)                                                                       'Errorhandling for user debugging
             Console.ReadLine()
         End Try
     End Sub
@@ -103,11 +120,15 @@ Module Module1
             Dim HrefMatch As Match = HrefRegex.Match(strSource)                                                      'Find matches in String
             While HrefMatch.Success = True                                                                           'String Found = True 1 or more strings matching
                 Dim pUrl As String = HrefMatch.Groups(1).Value                                                       'get first match
-                If pUrl.Contains(spiderurl) AndAlso Not pUrl.Contains("page") AndAlso Not pUrl.Contains("#") Then aReturn.Add(pUrl) 'precheck if link contains everything we want and fill the ReturnArray
+                If pUrl.Contains(spiderurl) AndAlso Not pUrl.Contains("page") AndAlso Not pUrl.Contains("#") Then
+                    aReturn.Add(pUrl)                                                                                'precheck if link contains everything we want and fill the ReturnArray
+                    'Console.WriteLine(pUrl, nl, nl)
+                End If
+                'Console.WriteLine(pUrl, nl & nl)                                                                    'only for debug
                 HrefMatch = HrefMatch.NextMatch                                                                      'go to next match
             End While
         Catch ex As Exception
-            Console.WriteLine(ex.Message, Environment.NewLine)                                                       'Errorhandling for user debugging
+            Console.WriteLine(ex.Message, nl)                                                                        'Errorhandling for user debugging
             Console.ReadLine()
         End Try
         Return aReturn
@@ -117,26 +138,61 @@ Module Module1
         Dim aReturn As String = Nothing
         Try
             Dim ti As TextInfo = CultureInfo.CurrentCulture.TextInfo
-            '           {http://www.movie-blog.org/2017/02/01/(.*?)-[1-2]{1}[0-9]{3}-.*?}
             'example url http://www.movie-blog.org/2017/02/01/kung-fu-killer-german-dts-dl-2014-1080p-bluray-x264-leethd-2/
-            'regexstr [1-2]{1}[0-9]{3} find 4 digits 1666 2999 1587 for year
-            Dim strRegex As String = spiderurl & "(.*?\-[1-2]{1}[0-9]{3})\-.*?"                                     'with year
+            Dim strRegexY As String = spiderurl & "(.*?\-[1-2]{1}[0-9]{3})\-.*?"                                     'with year    'regexstr [1-2]{1}[0-9]{3} find 4 digits 1666 2999 1587 for year
             'Dim strRegex As String = spiderurl & "(.*?)\-[1-2]{1}[0-9]{3}\-.*?"                                    'without year
-            Dim HrefRegex As New Regex(strRegex, RegexOptions.IgnoreCase Or RegexOptions.Compiled)                  'regex options to find the string in response 
-            Dim HrefMatch As Match = HrefRegex.Match(str)                                                           'Find matches in String
-            If HrefMatch.Success = True Then                                                                        'String Found = True 1 or more strings matching
-                Dim regStr As String = HrefMatch.Groups(1).Value                                                    'get match
-                'regStr = regStr.Substring(0, 1).ToUpper() & regStr.Substring(1)
-                regStr = regStr.Replace("ae", "ä").Replace("oe", "ö").Replace("ue", "ü").Replace("-", " ")          'replace chars
-                aReturn = ti.ToTitleCase(regStr)                                                                    'Word correction (uppercase chars)
+
+            'example url http://www.movie-blog.org/2017/02/03/ein-riskanter-plan-german-dl-1080p-bluray-x264-sons-2/
+            Dim strRegexG As String = spiderurl & "(.*?)\-german\-.*?"                                              'if not contains a Year
+
+            Dim HrefRegexY As New Regex(strRegexY, RegexOptions.IgnoreCase Or RegexOptions.Compiled)                  'regex options to find the string in response 
+            Dim HrefMatchY As Match = HrefRegexY.Match(str)                                                           'Find matches in String
+            If HrefMatchY.Success = True Then                                                                        'String Found = True 1 or more strings matching
+                Dim regStrY As String = HrefMatchY.Groups(1).Value                                                    'get match
+                regStrY = regStrY.Replace("ae", "ä").Replace("oe", "ö").Replace("ue", "ü").Replace("-", " ")          'replace chars
+                aReturn = ti.ToTitleCase(regStrY)                                                                    'Word correction (uppercase chars)
                 Return aReturn
+            Else
+                Dim HrefRegexG As New Regex(strRegexG, RegexOptions.IgnoreCase Or RegexOptions.Compiled)                  'regex options to find the string in response 
+                Dim HrefMatchG As Match = HrefRegexG.Match(str)                                                           'Find matches in String
+                If HrefMatchG.Success = True Then                                                                        'String Found = True 1 or more strings matching
+                    Dim regStrG As String = HrefMatchG.Groups(1).Value                                                    'get match
+                    regStrG = regStrG.Replace("ae", "ä").Replace("oe", "ö").Replace("ue", "ü").Replace("-", " ")          'replace chars
+                    aReturn = ti.ToTitleCase(regStrG)                                                                    'Word correction (uppercase chars)
+                    'Console.WriteLine(aReturn & nl, nl)
+                    Return aReturn
+                Else
+                End If
             End If
         Catch ex As Exception
-            Console.WriteLine(ex.Message, Environment.NewLine)                                                      'Errorhandling for user debugging
+            Console.WriteLine(ex.Message, nl)                                                                       'Errorhandling for user debugging
             Console.ReadLine()
         End Try
         Return aReturn
     End Function
+
+    Private Sub xmlTesting()
+        Dim settingPath = My.Application.Info.DirectoryPath & "\settings.xml"
+        If System.IO.File.Exists(settingPath) Then
+            Dim settingsRead As XmlReader = New XmlTextReader(settingPath)
+            While (settingsRead.Read())
+                Dim type = settingsRead.NodeType
+                If (type = XmlNodeType.Element) Then
+                    If (settingsRead.Name = "quality") Then
+                        Console.WriteLine(settingsRead.ReadInnerXml.ToString(), nl)
+                    End If
+                    If (settingsRead.Name = "source") Then
+                        Console.WriteLine(settingsRead.ReadInnerXml.ToString(), nl)
+                    End If
+                    If (settingsRead.Name = "series") Then
+                        Console.WriteLine(settingsRead.ReadInnerXml.ToString(), nl)
+                    End If
+                End If
+            End While
+        Else
+            Console.WriteLine("The filename you selected was not found.", nl)
+        End If
+    End Sub
 
     'Private Function getRealName(ByVal url As String)  'dont work only for testing
     '    Dim aReturn As String = Nothing
@@ -157,7 +213,7 @@ Module Module1
     '            Return aReturn
     '        End If
     '    Catch ex As Exception
-    '        Console.WriteLine(ex.Message, Environment.NewLine)                                                       'Errorhandling for user debugging
+    '        Console.WriteLine(ex.Message, nl)                                                       'Errorhandling for user debugging
     '        Console.ReadLine()
     '    End Try
     '    Return aReturn
@@ -172,11 +228,11 @@ Module Module1
     '        listRequest.Cx = sEngine
     '        Dim search As Search = listRequest.Execute()
     '        For Each item In search.Items
-    '            'Console.WriteLine("Title : " + item.Title + Environment.NewLine + "Link : " + item.Link + Environment.NewLine + Environment.NewLine)
+    '            'Console.WriteLine("Title : " + item.Title + nl + "Link : " + item.Link + nl + nl)
     '            Return item.Title
     '        Next
     '    Catch ex As Exception
-    '        Console.WriteLine(ex.Message, Environment.NewLine)                                       'Errorhandling for user debugging
+    '        Console.WriteLine(ex.Message, nl)                                       'Errorhandling for user debugging
     '        Console.ReadLine()
     '    End Try
     '    Return aReturn
