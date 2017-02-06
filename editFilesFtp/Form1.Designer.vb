8a42b29112e8f3ab4f8888ac416def5910876194
Hardy <haxxorhardy@gmail.com>
2017-02-06 17:15:29 +0100

    -added a Simple FTP-Tool to edit your release.txt on Server

---
+++ editFilesFtp/Form1.Designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.CheckedListBox1 = New System.Windows.Forms.CheckedListBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.buttonAdd = New System.Windows.Forms.Button()
+        Me.buttonRemove = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'CheckedListBox1
+        '
+        Me.CheckedListBox1.FormattingEnabled = True
+        Me.CheckedListBox1.Location = New System.Drawing.Point(12, 69)
+        Me.CheckedListBox1.Name = "CheckedListBox1"
+        Me.CheckedListBox1.Size = New System.Drawing.Size(294, 229)
+        Me.CheckedListBox1.TabIndex = 1
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(13, 13)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(293, 20)
+        Me.TextBox1.TabIndex = 2
+        '
+        'buttonAdd
+        '
+        Me.buttonAdd.Location = New System.Drawing.Point(13, 40)
+        Me.buttonAdd.Name = "buttonAdd"
+        Me.buttonAdd.Size = New System.Drawing.Size(75, 23)
+        Me.buttonAdd.TabIndex = 3
+        Me.buttonAdd.Text = "+ Add"
+        Me.buttonAdd.UseVisualStyleBackColor = True
+        '
+        'buttonRemove
+        '
+        Me.buttonRemove.Location = New System.Drawing.Point(231, 39)
+        Me.buttonRemove.Name = "buttonRemove"
+        Me.buttonRemove.Size = New System.Drawing.Size(75, 23)
+        Me.buttonRemove.TabIndex = 4
+        Me.buttonRemove.Text = "- Remove"
+        Me.buttonRemove.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(318, 310)
+        Me.Controls.Add(Me.buttonRemove)
+        Me.Controls.Add(Me.buttonAdd)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.CheckedListBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Edit my File"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents CheckedListBox1 As CheckedListBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents buttonAdd As Button
+    Friend WithEvents buttonRemove As Button
+End Class
---
+++ editFilesFtp/Form1.vb
@@ -0,0 +1,148 @@
+﻿Imports System.ComponentModel
+Imports System.Globalization
+Imports System.IO
+Imports System.Xml
+
+Public Class Form1
+
+#Region "Dims"
+    Dim nl As String = Environment.NewLine
+#End Region
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim settings As New ArrayList
+        settings = settingHelper()
+        Dim getList As New ArrayList
+        getList = getFile(settings)
+
+        fillBox(getList)
+    End Sub
+
+    Private Sub fillBox(ByVal list As ArrayList)
+        For i As Integer = 0 To list.Count - 1
+            Dim sHelp As String = list(i)
+            If sHelp.Contains("#") Then
+                sHelp = sHelp.Replace("#", "")
+                CheckedListBox1.Items.Insert(i, sHelp)
+                CheckedListBox1.SetItemChecked(i, True)
+            Else
+                CheckedListBox1.Items.Insert(i, sHelp)
+                CheckedListBox1.SetItemChecked(i, False)
+            End If
+        Next
+    End Sub
+
+
+    Private Function getFile(ByVal settings As ArrayList)
+
+        Dim aReturn As New ArrayList
+        Try
+            Dim pathFile As String = My.Application.Info.DirectoryPath & "\" & settings(0)
+            Dim wc As New Net.WebClient
+            My.Computer.Network.DownloadFile(settings(1) & settings(0), pathFile, settings(2), settings(3)) 'get file from Ftpserver with ans dave in directory
+            'My.Computer.Network.UploadFile("Dateipfad", "ftp://deineFTPseite.com/Dateiname+Endung", "Username", "Passwort")
+
+            'now read the file and fill aReturn
+            Dim txtReader As New StreamReader(pathFile)
+            Dim ti As TextInfo = CultureInfo.CurrentCulture.TextInfo
+            Dim sLine As String = Nothing
+            Do
+                sLine = txtReader.ReadLine()
+                If Not sLine Is Nothing Then
+                    sLine = ti.ToTitleCase(sLine.Replace("ä", "ae").Replace("ö", "oe").Replace("ü", "ue").Replace("ß", "ss").Replace("-", " "))
+                    aReturn.Add(sLine)
+                End If
+            Loop Until sLine Is Nothing
+            txtReader.Close()
+            Return aReturn
+        Catch ex As Exception
+            Debug.WriteLine(ex.ToString(), nl)
+        End Try
+        Return aReturn
+    End Function
+
+
+
+    Private Function settingHelper()            'read the setting.xml file
+        Dim aReturn As New ArrayList
+        Try
+            Dim settingPath = My.Application.Info.DirectoryPath & "\settings.xml"   'path to file
+            Dim settings As New XmlDocument()
+            Dim nodelist As XmlNodeList
+            Dim node As XmlElement
+            settings.Load(settingPath)                                      'load the file
+            nodelist = settings.SelectNodes("/settings/options")            'Xml-tree
+            For Each node In nodelist
+                aReturn.Add(node.Attributes.GetNamedItem("settingFile").Value.ToString)
+                aReturn.Add(node.SelectSingleNode("ftpserver").InnerText)
+                aReturn.Add(node.SelectSingleNode("username").InnerText)
+                aReturn.Add(node.SelectSingleNode("password").InnerText)
+                Debug.WriteLine("filename: " & aReturn(0) & nl & "Ftpserver: " & aReturn(1) & nl & "Username: " & aReturn(2) & nl & "Password: " & aReturn(3), nl)
+            Next
+        Catch ex As Exception
+            Debug.WriteLine(ex.ToString(), nl)
+        End Try
+        Return aReturn
+    End Function
+
+    Private Sub buttonAdd_Click(sender As Object, e As EventArgs) Handles buttonAdd.Click
+        Try
+            CheckedListBox1.Items.Insert(CheckedListBox1.Items.Count, TextBox1.Text)
+            CheckedListBox1.SetItemChecked(CheckedListBox1.Items.Count - 1, True)
+            TextBox1.Text = Nothing
+        Catch ex As Exception
+            Debug.WriteLine(ex.ToString(), nl)
+        End Try
+    End Sub
+
+    Private Function getItems()
+        Dim aReturn As New ArrayList
+        Try
+            For Each itemChecked In CheckedListBox1.CheckedItems
+                aReturn.Add(itemChecked.ToString)
+            Next
+        Catch ex As Exception
+            Debug.WriteLine(ex.ToString(), nl)
+        End Try
+        Return aReturn
+    End Function
+
+    Private Sub writeList(ByVal list As ArrayList, ByVal settings As ArrayList)
+        Try
+            Dim pathFile As String = My.Application.Info.DirectoryPath & "\" & settings(0)
+            Dim txtWriter As New StreamWriter(pathFile)
+            For Each item As String In list
+                txtWriter.WriteLine(item & "#")
+            Next
+            txtWriter.Close()
+        Catch ex As Exception
+            Debug.WriteLine(ex.ToString(), nl)
+        End Try
+    End Sub
+
+    Private Function setFile(ByVal settings As ArrayList)
+        Dim aReturn As Boolean = False
+        Try
+            Dim pathFile As String = My.Application.Info.DirectoryPath & "\" & settings(0)
+            Dim wc As New Net.WebClient
+            If My.Computer.Network.UploadFile(pathFile, settings(1) & settings(0), settings(2), settings(3)) Then
+                aReturn = True
+            End If
+        Catch ex As Exception
+            Debug.WriteLine(ex.ToString(), nl)
+        End Try
+        Return aReturn
+    End Function
+    Private Sub Form1_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        Dim settings As New ArrayList
+        settings = settingHelper()
+        Dim saveItems As New ArrayList
+        saveItems = getItems()
+        writeList(saveItems, settings)
+        If setFile(settings) Then
+            MessageBox.Show("Uploading Finished!", "Done", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Else
+            MessageBox.Show("Somthing went wrong!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1)
+        End If
+    End Sub
+End Class
